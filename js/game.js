e180da4858dda4bbb59e34fa8fbd1427349fd69e
Wisly ong <52624933+InsiderJanggo@users.noreply.github.com>
2020-03-23 07:10:30 -0700

    added tag and random color function

--- js/game.js
+++ js/game.js
@@ -1,3 +1,13 @@
+//Make The Background Color Random
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+     console.log(bgColor);
 
-
-
+     document.body.style.background = bgColor;
+}
+//Executing The Function
+random_bg_color();
+//
